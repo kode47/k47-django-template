a3c5b90d9fd066c46b73207ae86d67a754efd6b3
Kudzai Nyandoro <kudzain@gmail.com>
2019-04-06 10:55:26 -0700

    Add static pages.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html', {'title': 'Home'})
+
+def about(request):
+    return render(request, 'pages/about.html', {'title': 'About'})
+
+def contact(request):
+    return render(request, 'pages/contact.html', {'title': 'Contact'})
+
