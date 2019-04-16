ded12a78bc145ca5711761f488a7737e243ac08c
Kudzai Nyandoro <kudzain@gmail.com>
2019-04-16 07:49:39 -0700

    Quick update

--- template_project/settings.py
+++ template_project/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+
 
 ALLOWED_HOSTS = ['https://k47temp.herokuapp.com']
 
